e9c0403745313e2d8a19c887d22d452959859763
egemr <blncksvc2828@gmail.com>
2022-05-29 23:43:58 +0300

    başlık ekleme

--- app/src/main/java/com/emrckmk/myapplication/MainActivity.kt
+++ app/src/main/java/com/emrckmk/myapplication/MainActivity.kt
@@ -2,10 +2,40 @@ package com.emrckmk.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.view.*
+import kotlinx.android.synthetic.main.navigation_baslik.view.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        NavigationUI.setupWithNavController(navigationView, navHostFragment.navController)
+
+        toolbar.title ="Başlık"
+        val toogle=ActionBarDrawerToggle(this,drawer,toolbar,0,0)
+        drawer.addDrawerListener(toogle)
+        toogle.syncState()
+
+        val baslik= navigationView.inflateHeaderView(R.layout.navigation_baslik)
+        baslik.textView3.text ="Merhaba"
+
+    }
+
+    override fun onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START)
+        }else{
+            super.onBackPressed()
+        }
+
     }
 }
